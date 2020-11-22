6a13d9c0e3ae5bc51db5a186ca3559a62a5d512e
Phillip Tran <pntran@ucdavis.edu>
2020-11-21 17:37:48 -0800

    testing pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing our pipeline!!
+          Phillip was here
         </p>
         <a
           className="App-link"
